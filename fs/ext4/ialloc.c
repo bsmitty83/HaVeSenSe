b3b6bf7356264e294202682a62aded7a42042397
Nick Reuter <nreuter85@gmail.com>
2013-04-05 14:23:42 -0500

    Linux-3.0.72
    Change-Id: Id111846477bda292f60036507821d1d431096553
    
    Conflicts:
    	drivers/bluetooth/btusb.c
    	fs/ext4/ialloc.c

--- fs/ext4/ialloc.c
+++ fs/ext4/ialloc.c
@@ -358,7 +358,6 @@ static int find_group_flex(struct super_block *sb, struct inode *parent,
 find_close_to_parent:
 	flexbg_free_blocks = atomic_read(&flex_group[best_flex].free_blocks);
 	flex_freeb_ratio = div64_u64(flexbg_free_blocks * 100, blocks_per_flex);
-	if (atomic_read(&flex_group[best_flex].free_inodes) &&
 	    flex_freeb_ratio > free_block_ratio)
 		goto found_flexbg;
 
@@ -373,6 +372,8 @@ find_close_to_parent:
 
 		flexbg_free_blocks = atomic_read(&flex_group[i].free_blocks);
 		flex_freeb_ratio = div64_u64(flexbg_free_blocks * 100, blocks_per_flex);
+		flexbg_free_blocks = atomic64_read(&flex_group[i].free_blocks);
+		flex_freeb_ratio = flexbg_free_blocks * 100 / blocks_per_flex;
 
 		if (flex_freeb_ratio > free_block_ratio &&
 		    (atomic_read(&flex_group[i].free_inodes))) {
